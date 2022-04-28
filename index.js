ac816ccd82c014a8d67948ee3b60c70fb3d8b9b7
VLSLV333 <v.sherstyk@gmail.com>
2022-04-28 17:08:25 +0000

    created an array of numbers out of integer (all numbers seperate)

---
+++ index.js
@@ -0,0 +1,26 @@
+// function v (num){
+//     let j = Array.from(String(num), Number);        // converting integer into Array (each number separate)
+
+// //     console.log(j)
+//     // console.log(j[0] + j[1] + j[2])
+//     console.log(j.reduce( (x,y) => x + y,0))
+//     // let acc = 0
+//     // for (key of j){
+//     //     acc += key
+//     // }
+//     // console.log(acc)
+// }
+
+// function v (num){
+//     if (num <= 0 || num >= 13){
+//         console.log("Enter right month")
+//     } else if (num === 12 || num <= 2){
+//         console.log("Winter")
+//     } else if (num <= 5){
+//         console.log("Spring")
+//     } else if (num <= 8){
+//         console.log("Summer")
+//     } else {
+//         console.log("Autumn")
+//     }
+// }
